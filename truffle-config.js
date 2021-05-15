1a491e55c9c7fb08cb74f1c5dc289274701ad9ee
Gonzalo <gonzacalleja@gmail.com>
2021-05-15 13:12:21 +0200

    Finish adding test networks to deploy smart contract

--- truffle-config.js
+++ truffle-config.js
@@ -6,27 +6,37 @@ const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
   networks: {
+    // Local Ganache Blockchain used for testing deployment
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*", // Match any network id
-      from: deployer
+      from: deployer // ensuring the deployer is the same account for all networks
     },
+
+    // Test Networks to make smart contract publicly available
+    // They work just like the main net, but the Ether is worthless
+    // In order for deployment to work first request funds from a faucet!
     kovan: {
-      provider: () => new HDWalletProvider(mnemonic, `https://kovan.infura.io/v3/${projectId}`),
+      provider: () => new HDWalletProvider({
+        mnemonic: mnemonic, 
+        providerOrUrl: `https://kovan.infura.io/v3/${projectId}`, 
+        addressIndex: 0
+      }),
       network_id: 42,
       gas: 5000000,
       gasPrice: 20000000000,
       confirmations: 2,
       timeoutBlocks: 200,
       skipDryRun: true,
-      from: deployer
     },
 
     ropsten: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${projectId}`);
-      },
+      provider: () => new HDWalletProvider({
+        mnemonic: mnemonic, 
+        providerOrUrl: `https://ropsten.infura.io/v3/${projectId}`, 
+        addressIndex: 0
+      }),
       network_id: 3,
       gasPrice: 20000000000,
       gas: 5000000,
