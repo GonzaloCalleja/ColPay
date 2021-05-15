0370bf497ff88acb5e2617c6532f9287ff60b9e0
Gonzalo <gonzacalleja@gmail.com>
2021-05-15 19:09:42 +0200

    requestTokens function added

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,22 @@
+const CPToken = artifacts.require('CPToken')
 const ColPay = artifacts.require('ColPay')
 
 module.exports = async function(deployer, network, accounts) {
+  // Deploy ColPay Token
+  await deployer.deploy(CPToken)
+  const cpToken = await CPToken.deployed()
+  
+  
   // Deploy Contract
-  //await 
-  deployer.deploy(ColPay)
-  //const daiToken = await DaiToken.deployed()
+  await deployer.deploy(ColPay, cpToken.address)
+  const colPay = await ColPay.deployed()
 
-  // Call any necessary functions on contract migration
-  //await ColPay.aFunction()
+  // Transfer all ColPay Tokens tokens to ColPay (1 million)
+  await cpToken.transfer(colPay.address, '1000000000000000000000000')
+
+  // Transfer 100 ColPay Tokens to initial buyer account
+  await colPay.requestTokens('100000000000000000000', {from: accounts[1]})
+
+  // Transfer 10 ColPay Tokens to initial seller account
+  await colPay.requestTokens('10000000000000000000', {from: accounts[2]})
 }
--- src/contracts/ColPay.sol
+++ src/contracts/ColPay.sol
@@ -1,15 +1,48 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
+import "./CPToken.sol" as CPToken;
+
 contract ColPay {
     string public name;
+    address public owner;
+    CPToken.CPToken public cpToken;
+
+    mapping(uint => PaymentContract) public paymentContracts;
+    mapping(address => uint[]) public paymentContractsHeldPerAddress;
 
     struct Transaction {
-        uint id;
+        uint256 date;
+        uint value;
     }
 
-    constructor() {
+    struct PaymentContract {
+        uint id;
+        string name;
+        uint totalAmount;
+        uint amountPayed;
+        address buyer;
+        address payable seller;
+        Transaction[] transactions;
+        uint256 startDate;
+        uint256 expiryDate;
+        bool completed;
+    }
+
+    constructor(CPToken.CPToken _cpToken) {
         name = "Payment Fragmentation Service";
+        cpToken = _cpToken;
+        owner = msg.sender;
+    }
+
+    // Request Tokens - for new accounts
+    function requestTokens(uint256 _value) public {
+        // check for valid requests
+        require(_value > 0, "Amount Requested Cannot be 0");
+        require(_value < cpToken.balanceOf(address(this))/10, "amount cannot be larger than 10% of current supply");
+        require(cpToken.balanceOf(msg.sender) == 0 && paymentContractsHeldPerAddress[msg.sender].length == 0, "only issue tokens to new accounts");
+
+        cpToken.transfer(msg.sender, _value);
     }
 
     // Create Transactions
