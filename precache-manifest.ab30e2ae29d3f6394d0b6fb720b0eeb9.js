033d2770f73b4f76e538cac54d3901129d30c307
Gonzalo <gonzacalleja@gmail.com>
2021-05-15 16:35:58 +0200

    Updates

---
+++ precache-manifest.ab30e2ae29d3f6394d0b6fb720b0eeb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e12ff05a3b73c452eba4",
+    "url": "/ColPay/static/css/main.05bf602c.chunk.css"
+  },
+  {
+    "revision": "e12ff05a3b73c452eba4",
+    "url": "/ColPay/static/js/main.e12ff05a.chunk.js"
+  },
+  {
+    "revision": "7c0ee825135195ebaec2",
+    "url": "/ColPay/static/css/1.c30bd570.chunk.css"
+  },
+  {
+    "revision": "7c0ee825135195ebaec2",
+    "url": "/ColPay/static/js/1.7c0ee825.chunk.js"
+  },
+  {
+    "revision": "a3c18c2fd5870b48798d",
+    "url": "/ColPay/static/js/runtime~main.a3c18c2f.js"
+  },
+  {
+    "revision": "16d4401f934c35e5415e93e56eb4188f",
+    "url": "/ColPay/index.html"
+  }
+];
